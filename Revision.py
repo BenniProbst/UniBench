ea560a2b458a378cce655e167ce5df3cac0f7c3f
benjamin-elias <benni.probst@gmx.de>
2021-03-11 20:06:44 +0100

    revision can download git

--- Revision.py
+++ Revision.py
@@ -6,11 +6,30 @@ class Revision:
     suites = ''
     tests = ''
 
-    def revision_setup(self, working_directory, program_url, username, password):
+    def revision_setup(self, working_directory, program_url, username, password, project_name):
         while True:
             git_branch = input('Git branch: ')
             git_code = input('Git revision code: ')
-            cmd1 = ['git', 'clone', '-n', program_url, ]
+            cmd0 = ['cd', '\"' + working_directory + '\"']
+            cmd1 = ['git', 'clone', '-n', program_url,
+                    working_directory + '/' + project_name + '-' + git_branch + '-' + git_code]
+            cmd2 = ['git', 'checkout', git_code]
+            process = subprocess.Popen(cmd0, shell=True, stdout=subprocess.PIPE)
+            process.wait()
+            out = []
+            for line in process.stdout:
+                out.append(line)
+            process = subprocess.Popen(cmd1, shell=True, stdout=subprocess.PIPE)
+            process.wait()
+            out = []
+            for line in process.stdout:
+                out.append(line)
+            process = subprocess.Popen(cmd2, shell=True, stdout=subprocess.PIPE)
+            process.wait()
+            out = []
+            for line in process.stdout:
+                out.append(line)
+
             rep = input('Add another revision code for testing? (y/n)')
             while not (rep == 'y' or rep == 'n'):
                 print('What did you say?')
@@ -18,7 +37,7 @@ class Revision:
             if rep == 'n':
                 break
 
-    def p_name(self,url):
+    def p_name(self, url):
         if str(url).endswith('.git'):
             url = str(url).removesuffix('.git')
         name = url.split("/")
@@ -34,10 +53,9 @@ class Revision:
             print('Set platform login:')
             program_url = input('Git URL:')
             username = input('Target platform username: ')
-            print('Target platform password: ')
-            password = getpass()
+            password = getpass.getpass(prompt='Target platform password: ')
             project_name = self.p_name(program_url)
-            self.revision_setup(working_directory, program_url, username, password)
+            self.revision_setup(working_directory, program_url, username, password, project_name)
             rep = input('Add another revision source? (y/n)')
             while not (rep == 'y' or rep == 'n'):
                 print('What did you say?')
