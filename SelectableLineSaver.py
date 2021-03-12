1f9468e6e0e2d397f4e0411bd2af795e61a9832c
benjamin-elias <benni.probst@gmx.de>
2021-03-12 23:48:14 +0100

    configure single commands

--- SelectableLineSaver.py
+++ SelectableLineSaver.py
@@ -6,7 +6,7 @@ import os
 
 class SelectableLineSaver(LineSaver):
     target_run = Path(os.path.expanduser("~") + '/noname.UniBench_config_run')
-    in_memory_run = []
+    in_memory_run = ['']
 
     def __init__(self, t_f):
         super().__init__(self, t_f)
@@ -18,15 +18,58 @@ class SelectableLineSaver(LineSaver):
 
     def help(self):
         print('HELP:')
-        print('add \"command\" - add an executeable command line')
-        print('remove index - remove an executeable command line from list above')
-        print('add \"command\",\"command\",... - add multiple executeable command lines')
-        print('remove index,index,... - remove multiple executeable command lines from list above.')
-        print('save_run index,index,... - save run order of commands above')
+        print('add \"command\" - add an executable command line')
+        print('remove index - remove an executable command line from list above')
+        print('add \"command\",\"command\",... - add multiple executable command lines')
+        print('remove index,index,... - remove multiple executable command lines from list above.')
+        print('save_run index,index,... - save run order of commands above, save 0 to run nothing')
         print('run - run configured command order')
+        print('help - print help once again')
 
-    def configure(self):
+    def status(self):
+        self.load_from_file()
+        self.load_from_file_run()
+        if len(self.in_memory) == 0:
+            print('No saved selectable commands. Please configure!')
+        else:
+            print("Saved commands:")
+            count = 1
+            for i in self.in_memory:
+                print(str(count) + ': \"' + str(i) + '\"')
+                count += 1
+        if len(self.in_memory_run) == 0:
+            print('No saved run order. Please configure!')
+        else:
+            print('\n')
+            print('Run order:')
+            print(self.in_memory_run[0])
+
+    def configure(self, cmd_list):
+        count = 1
+        for c in cmd_list:
+            self.append(c)
+            if len(self.in_memory_run[0]) == 0:
+                self.in_memory_run[0] += str(count)
+            else:
+                self.in_memory_run[0] += ',' + str(count)
+            count += 1
+        self.write_back_run()
         self.help()
+        self.status()
+        while True:
+            com = input('Configuration command: ')
+            if com.startswith('add'):
+                pass
+            elif com.startswith('remove'):
+                pass
+            elif com.startswith('save_run'):
+                pass
+            elif com.startswith('run'):
+                pass
+            elif com.startswith('help'):
+                pass
+            else:
+                print('Invalid command!')
 
     def load_from_file_run(self):  # discard changes
         with open(self.target_file_run, 'r') as f:
