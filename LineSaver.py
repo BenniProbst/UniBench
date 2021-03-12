6627d34f7533e7acebe6dd15b1b9985c4cc636cc
benjamin-elias <benni.probst@gmx.de>
2021-03-12 13:10:59 +0100

    LineSaver.py insert functions

--- LineSaver.py
+++ LineSaver.py
@@ -5,6 +5,7 @@ import pickle
 
 class LineSaver:
     target_file = Path(os.path.expanduser("~") + '/noname.UniBench_config')
+    in_memory = ['']
 
     def __init__(self, t_f):
         self.target_file = Path(t_f)
@@ -13,12 +14,31 @@ class LineSaver:
             os.makedirs(working_dir)
         if not os.path.isfile(self.target_file):
             with open(self.target_file, 'w') as f:
-                pickle.dump([''], f)
+                pickle.dump(self.in_memory, f)
+        self.in_memory = self.load_from_file()
 
-    def save(self, listing):
-        with open(self.target_file, 'wb') as f:
-            pickle.dump(listing, f)
-
-    def load(self):
+    def load_from_file(self):  # discard changes
         with open(self.target_file, 'rb') as f:
-            return pickle.load(f)
+            self.in_memory = pickle.load(f)
+        return self.in_memory
+
+    def write_back(self):
+        with open(self.target_file, 'wb') as f:
+            pickle.dump(self.in_memory, f)
+        return self.in_memory
+
+    def load_from_memory(self):
+        return self.in_memory
+
+    def save_to_memory(self, listing):
+        self.in_memory = listing
+
+    def append(self, thing):
+        self.in_memory = self.load_from_file()
+        self.in_memory.append(thing)
+        self.write_back()
+
+    def insert(self, thing, index):
+        self.in_memory = self.load_from_file()
+        self.in_memory.insert(index, thing)
+        self.write_back()
